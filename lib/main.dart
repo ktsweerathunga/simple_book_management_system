5d6127b1199ce20dacdbdb97cd37b4c05b206e4a
ktsweerathunga <135198812+ktsweerathunga@users.noreply.github.com>
2025-04-10 16:30:53 +0530

    Add main.dart with basic Flutter app entry

---
+++ lib/main.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+import 'home_page.dart';
+
+void main() {
+  runApp(const BookApp());
+}
+
+class BookApp extends StatelessWidget {
+  const BookApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: const HomePage(),
+    );
+  }
+}
